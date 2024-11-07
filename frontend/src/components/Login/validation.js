bdd9550f996efb9fb1770ff01706b914050105be
Ronish Katuwal <ronishkatuwal00@gmail.com>
2024-11-07 15:34:21 +0545

    password length validation

--- frontend/src/components/Login/validation.js
+++ frontend/src/components/Login/validation.js
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 
 const schema = yup.object({
     email: yup.string().email("Email is not valid").required("Email is required"),
-    password: yup.string().required("Password is required")
+    password: yup.string().required("Password is required").min(8, "Password length should be 8 minumum")
   })
 
   export default schema
--- frontend/src/components/SignUp/validation.js
+++ frontend/src/components/SignUp/validation.js
@@ -3,7 +3,7 @@ import * as yup from "yup";
 const schema = yup.object({
     fullname: yup.string().required("Fullname is required"),
     email: yup.string("Email is not valid").required("Email is required"),
-    password: yup.string().required("Password is required")
+    password: yup.string().required("Password is required").min(8, "Password length should be 8 minumum")
 })
 
 export default schema
