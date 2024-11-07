05c7ac108466dd6ac249deb4bd59e0fe76648b1b
Ronish Katuwal <ronishkatuwal00@gmail.com>
2024-11-07 13:57:05 +0545

    url

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,8 +10,8 @@ app.use(cors({
     credentials: true
 }));
 app.use(cookieParser());
-app.use(express.json({limit: '16kb'}));
-app.use(express.urlencoded({extended: true, limit: '16kb'}));
+app.use(express.json({limit: '50kb'}));
+app.use(express.urlencoded({extended: true, limit: '50kb'}));
 
 //import router
 import userRouter from './routes/user.route.js'
